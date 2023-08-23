e5ce258c061189d24747132238e41c6c151403aa
llNonlosoll <wishmasterar@gmail.com>
2023-08-23 10:59:31 +0200

    searchConditions added

--- controllers/contacts/getAllContacts.js
+++ controllers/contacts/getAllContacts.js
@@ -3,11 +3,16 @@ const { Contact } = require("../../models/contact");
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
 
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
 
   const skip = (page - 1) * limit;
 
-  const data = await Contact.find({ owner }, "-createdAt -updatedAt", {
+  const searchConditions = { owner };
+  if (favorite !== undefined) {
+    searchConditions.favorite = favorite;
+  }
+
+  const data = await Contact.find(searchConditions, "-createdAt -updatedAt", {
     skip,
     limit,
   }).populate("owner", "name email");
